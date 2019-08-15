5cd80a4066ad38a4546a95fcf8c1bcfeed4f3cb6
Anwesh43 <anweshthecool0@gmail.com>
2019-08-15 21:48:28 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -2,3 +2,27 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight
 const scGap : number = 0.05
 const delay : number = 50
+
+class State {
+
+    scale : number = 0
+    dir : number = 0
+    prevScale : number = 0
+
+    update(cb : Function) {
+        this.scale += this.dir * scGap
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
